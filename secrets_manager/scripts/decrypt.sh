c82989387a697a7161c1d734e2bdef18f0140afa
Shibu TN <tnshibu@yahoo.com>
2020-12-14 11:04:51 +0530

    a

---
+++ secrets_manager/scripts/decrypt.sh
@@ -0,0 +1,2 @@
+gpg --decrypt --batch --yes --output src/main/resources/secret.properties.txt src/main/resources/secret.properties.encrypted
+
---
+++ secrets_manager/scripts/encrypt.sh
@@ -0,0 +1,3 @@
+gpg --encrypt --batch --yes --armor -r tnshibu@yahoo.com          --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties
+gpg --encrypt --batch --yes --armor -r shibu.narayanan@oracle.com --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties
+
