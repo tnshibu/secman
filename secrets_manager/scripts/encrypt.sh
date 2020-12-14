a86bbf867e60d43bfef5019e302f0953bc8750e8
Shibu TN <tnshibu@yahoo.com>
2020-12-15 00:09:37 +0530

    a

--- secrets_manager/scripts/encrypt.sh
+++ secrets_manager/scripts/encrypt.sh
@@ -1,3 +1,12 @@
-gpg --encrypt --batch --yes --armor -r tnshibu@yahoo.com          --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties
-gpg --encrypt --batch --yes --armor -r shibu.narayanan@oracle.com --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties
+#gpg --encrypt --batch --yes --armor -r tnshibu@yahoo.com          --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties
+#gpg --encrypt --batch --yes --armor -r shibu.narayanan@oracle.com --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties
+#
 
+#echo gpg --encrypt --batch --yes --armor > ids_p1.txt
+echo | set /p dummyName=gpg --encrypt --batch --yes --armor > ids_p1.txt
+gpg  --list-keys --with-colons --fast-list-mode | awk -F: '/^pub/{printf "-r %s ", $5}' > ids_p2.txt
+echo --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties > ids_p3.txt
+
+cat ids_p1.txt ids_p2.txt ids_p3.txt > ids_final.bat
+ids_final.bat
+#gpg --encrypt --batch --yes --armor $(gpg --list-keys --with-colons --fast-list-mode | awk -F: '/^pub/{printf "-r %s ", $5}')  --output src/main/resources/secret.properties.encrypted src/main/resources/secret.properties
